dcdc147e701bb2e5544ec621e01adfddd5c269b2
That Dude <dan@xp11.com>
2022-09-22 15:26:32 +0100

    Updated readme

--- Emulated-Eastron-SDM/Emulated-Eastron-SDM.ino
+++ Emulated-Eastron-SDM/Emulated-Eastron-SDM.ino
@@ -1,11 +1,10 @@
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <SoftwareSerial.h>
 #include <ModbusRTU.h>
 #include "private.h"
 
-SoftwareSerial S(D3,D2); // we need one serial port for communicating with RS 485 to TTL adapter
+SoftwareSerial S(D3, D2);  // we need one serial port for communicating with RS 485 to TTL adapter
 
 #define SLAVE_ID 1
 
@@ -24,6 +23,45 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  S.begin(9600, SWSERIAL_8N1);  // software serial attached to the MAX485 module
+  mb.begin(&S, D0);             // RE/DE connected to D0 of ESP8266
+
+  mb.slave(SLAVE_ID);
+
+  // add a series of Input registers
+  mb.addIreg(0);
+  mb.addIreg(1);
+  mb.addIreg(2);
+  mb.addIreg(3);
+  mb.addIreg(4);
+  mb.addIreg(5);
+  mb.addIreg(6);
+  mb.addIreg(7);
+  mb.addIreg(8);
+  mb.addIreg(9);
+  mb.addIreg(10);
+  mb.addIreg(11);
+  mb.addIreg(12);
+  mb.addIreg(13);
+
+  // set Input register values
+  mb.Ireg(0, 250);   // line to neutral volts (integer-part)
+  mb.Ireg(1, 400);   // line to neutral volts (fractional-part)
+  mb.Ireg(2, 10);    // Current Amps  (integer-part)
+  mb.Ireg(3, 54);    // Current Amps (fractional part)
+  mb.Ireg(4, 2500);  // Active power Watts (integer part)
+  mb.Ireg(5, 76);    // Active power Watts (fractional part)
+  mb.Ireg(6, 2639);  // Apparent power VoltAmps (integer part)
+  mb.Ireg(7, 216);   // Apparent power VoltAmps (float part)
+  mb.Ireg(8, 2375);  // Reactive power VAr (integer part)
+  mb.Ireg(9, 5);     // Reactive power VAr (fractional part)
+  mb.Ireg(10, 0);    // Power factor (none) (integer part)
+  mb.Ireg(11, 90);   // Power factor (none) (fractional part)
+  mb.Ireg(12, 30);   // Phase angle (Degree) (integer-part) - ** this is not a
+                     // calculated value but my device doesnt actially use it
+  mb.Ireg(13, 1);    // Phase angle (Degree) (fractional-part) - ** this is not a
+                     // calculated value but my device doesnt actially use it
 }
 
 // **************************************************
@@ -48,12 +86,14 @@ void setup_wifi() {
 }
 
 // **************************************************
+// steps to take when a power value is recieved via MQTT
+
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(": ");
 
-  // convert each byte to a char and build a string containing the mqtt message, in this case a float
+  // convert each byte to a char and build a string containing the mqtt message
   String messageTemp;
   for (int i = 0; i < length; i++) {
     // Serial.print((char)message[i]);
@@ -61,22 +101,25 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
   Serial.println(messageTemp);  // print the topic message (it's a string at this point)
 
-  // convert the stirng (messageTemp) to a integer. I'm ignoring the fractional part so 123.456 will return 123
-  int x;
+  // convert the stirng (messageTemp) to an integer. I'm ignoring the fractional part so 123.456 will return 123
+  int power_in_watts;
   const char* string1 = messageTemp.c_str();
-  x = atoi(string1);
-  Serial.printf("Converted to int: %d \n", x);
+  power_in_watts = atoi(string1);
+  Serial.printf("Converted to int: %d \n", power_in_watts);
+
+  // update the Active power Watts (integer part)
+  mb.Ireg(4, power_in_watts);
 
   // If a message is received on the topic shellies/shellyem-house/emeter/0/power
-  if (String(topic) == mqtt_topic) {
-    //Serial.println("We have message!");
-    // determine is the power value is negative, ie we are exporting power to the grid
-    if (messageTemp == "on") {
-      Serial.println("on");
-    } else if (messageTemp == "off") {
-      Serial.println("off");
-    }
-  }
+  // if (String(topic) == mqtt_topic) {
+  //   //Serial.println("We have message!");
+  //   // determine is the power value is negative, ie we are exporting power to the grid
+  //   if (messageTemp == "on") {
+  //     Serial.println("on");
+  //   } else if (messageTemp == "off") {
+  //     Serial.println("off");
+  //   }
+  // }
 }
 
 // **************************************************
@@ -105,4 +148,6 @@ void loop() {
     reconnect();
   }
   client.loop();
+  mb.task();
+  yield();
 }
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Fake Eastron SDM
- An ESP8266 / 32 project to emulate an Eastron SDM power meter
+ An ESP8266 project to emulate an Eastron SDM power meter
+
+ ## Background
+
+ I need to limit the power that my solar inverter exports to the grid.
+ 
+ This is normally achieved by connecting an Eastron SDM230 power meter via modbus to the inverter's (Growatt MIN 3600TL-XE in my case) 2nd modbus port. Once installed the inverter acts as a master, polling the SDM230 slave device's input regisiters for data. If it detects that power is being exported to the grid, it limits the inverter's output power to maintain (approximately) zero export.
+
+ I have a slightly more complex setup with a solar water heater diverter in the mix, this intercepts the unused power and uses it to heat my hot water tank. But once the talk is fully heated (usually around 2pm in the Summer) the power is exported to the grid - which I do not want. The real SDM230 power meter is not compatible with this process and half export before the solar diverter gets a chance to heat the water.
+
+ My solution for this is to emulate the Eastron SDM230 and maintain full control of when the inverter limits is power output.
+
+ ## Alpha software
+
+ Heads up!
+ - I am not a dev.
+ - This is not yet tested as I'm away from home.
+ - I started developing this project out of necesity and learned just enough C++ to get this working, my code is probably horrible!
+
+ ## Required tools
+
+ You will need:
+-  ESP8266 device (I'm using a Wemos D1 mini clone 3.5 euros)
+-  MAX485 TTL to RS-485 Interface Module (2 euros)
+-  A calbe to connect the module to your inverter (I cut the ends of an Ethernet patch cable)
+-  5 dupont cables
+
+## Building the device
+
+It's super easy. Connect the MAX485 module to your ESP8266 board:
+
+D2	  ---> DI
+D3	  ---> RO
+D0	  ---> RE
+D0	  ---> DE
+Vcc	  ---> 3.3V
+GND   ---> GND
+
+Then connect your MAX485 module's A/B terminals to the A/B terminals on your inverter using two wires from your sacrificial ethernet patch cable - A links to A, B links to B. In my case, on the Growatt MIN 3600TL-XE I had to use pins 5 and 6 which is the power meter modbus.
